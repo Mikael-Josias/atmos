7acaef7b42cc53461277ad50f48223077a756774
Mikael Josias <mikael.josias01@gmail.com>
2025-02-28 13:29:39 -0300

    style(weather): change scrollbar

--- app/components/WeatherDashboard.tsx
+++ app/components/WeatherDashboard.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import CurrentWeatherSection from "./CurrentWeatherSection";
 import HourlyWeatherSection from "./HourlyWeatherSection";
 
 export default function WeatherDashboard() {
   return (
-    <section className="flex flex-col gap-6 p-6 bg-gray-100 w-full h-full overflow-auto">
-      <CurrentWeatherSection />
-      <HourlyWeatherSection />
-    </section>
+    <ScrollArea className="h-full w-full">
+      <section className="flex flex-col gap-6 p-6 bg-gray-100 w-full h-full overflow-auto">
+        <CurrentWeatherSection />
+        <HourlyWeatherSection />
+      </section>
+    </ScrollArea>
   );
 }
