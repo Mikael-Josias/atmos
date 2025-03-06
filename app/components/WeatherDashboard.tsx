4a343fef1e8cbb05087f7df840e7c9cc0336c86f
Mikael Josias <mikael.josias01@gmail.com>
2025-03-06 16:01:17 -0300

    style(dashboard): change container sizer

--- app/components/WeatherDashboard.tsx
+++ app/components/WeatherDashboard.tsx
@@ -5,8 +5,8 @@ import HourlyWeatherSection from "./HourlyWeatherSection";
 
 export default function WeatherDashboard() {
   return (
-    <ScrollArea className="h-full w-full">
-      <section className="flex flex-col gap-6 p-6 bg-gray-100 dark:bg-gray-700 w-full h-full overflow-auto">
+    <ScrollArea className="h-full md:h-screen bg-gray-100 dark:bg-gray-700 w-full">
+      <section className="flex flex-col gap-6 p-6 w-full h-full overflow-auto">
         <CurrentWeatherSection />
         <HourlyWeatherSection />
       </section>
