1e6e3069736858ac87e573380f530273a7af9846
mikael josias <mikaeljosias2002@gmail.com>
2025-03-27 19:40:37 -0300

    feat(weather): add loading state on days carousel

--- app/components/DaysCarousel/DayCard.tsx
+++ app/components/DaysCarousel/DayCard.tsx
@@ -1,8 +1,9 @@
 import { useWeatherContext, WeatherHourly } from "@/app/contexts/WeatherContext"
 import { WEATHER_ICONS_SMALL } from "@/app/utils/utils"
+import { Loader2 } from "lucide-react"
 
 export default function DayCard({ weather }: { weather: WeatherHourly }) {
-  const { selectedWeather, setSelectedWeather } = useWeatherContext()
+  const { selectedWeather, loadingWeather, setSelectedWeather } = useWeatherContext()
   const thisHour = new Date().getHours()
 
   function selectNewDay() {
@@ -21,7 +22,11 @@ export default function DayCard({ weather }: { weather: WeatherHourly }) {
 
   return (
     <div className={`${isSelected() ? "bg-white shadow-sm" : "hover:bg-snow_500"} flex items-center justify-center gap-3 flex-shrink-0 p-4 md:p-3 first:pl-6 last:pr-6 cursor-pointer transition-all`} onClick={() => selectNewDay()}>
-      {WEATHER_ICONS_SMALL.get(weather.weather_code[thisHour])}
+      {loadingWeather ?
+        <Loader2 className="w-8 animate-spin text-cement_400" />
+        :
+        WEATHER_ICONS_SMALL.get(weather.weather_code[thisHour])
+      }
       <div className="flex flex-col">
         <span className="font-alexandria text-xs font-light text-cement_500 capitalize">{DAYS[new Date(weather.time[0]).getDay()]}</span>
         <span className="font-alexandria text-xs font-light text-cement_400">{new Date(weather.time[0]).getDate()}/{new Date(weather.time[0]).getMonth() < 10 ? `0${new Date(weather.time[0]).getMonth()}` : new Date(weather.time[0]).getMonth()}</span>
